0e46ae61cac9a917c72dffd6b7b9c38b308d7df2
NguyenVanThangzzzz <106645703+NguyenVanThangzzzz@users.noreply.github.com>
2023-06-11 05:31:59 +0700

    update all

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,7 @@
 import pool from "../configs/connectDB";
 import fs from 'fs';
 import multer from 'multer';
+import path from 'path';
 var appRoot = require('app-root-path');
 
 
@@ -12,13 +13,13 @@ let getHomepage = async (req, res) => {
 }
 let getProductDetailPage = async (req, res) => {
     let productId = req.params.id;
-    let [product] = await pool.execute(`select * from products where id = ?`, [productId ]);
+    let [product] = await pool.execute(`select * from products where id = ?`, [productId]);
     return res.send(JSON.stringify(product))
 }
 let createNewProduct = async (req, res) => {
     let { name, infomation, price } = req.body;
     let image = req.file.filename; // Lấy tên file ảnh đã tải lên
-  
+
 
     // Di chuyển file ảnh vào thư mục public/image trên máy chủ
     fs.renameSync(req.file.path, appRoot + "/src/public/image/" + image);
@@ -34,9 +35,41 @@ let createNewProduct = async (req, res) => {
 
 let getCreateProductPage = (req, res) => {
     return res.render('createproducts.ejs'); // Render trang tạo sản phẩm
-  };
+};
+let getAdminproduct = async (req, res) => {
+    const [rows, fields] = await pool.execute('SELECT * FROM products');
 
+    return res.render('adminproduct.ejs', { dataProduct: rows })
 
+}
+let deleteProduct = async (req, res) => {
+    let productId = req.body.productId;
+    await pool.execute('delete from products where id = ?', [productId])
+    return res.redirect("/Adminproduct");
+}
+let getEditProduct = async (req, res) => {
+    let id = req.params.id;
+    let [product] = await pool.execute('Select * from products where id = ?', [id]);
+    return res.render('updateproducts.ejs', { dataProduct: product[0] }); // x <- y
+}
+let postUpdateProduct = async (req, res) => {
+    let { name, infomation, price, id } = req.body;
+    let image = req.file ? req.file.filename : ""; // Gán giá trị mặc định nếu không có tệp ảnh
+
+    // Kiểm tra xem tệp ảnh đã tải lên thành công hay chưa
+    if (req.file) {
+        // Di chuyển tệp ảnh vào thư mục public/image trên máy chủ
+        fs.renameSync(req.file.path, path.join(appRoot.path, "/src/public/image/", image));
+    }
+
+    // Cập nhật thông tin sản phẩm trong cơ sở dữ liệu
+    await pool.execute(
+        'UPDATE products SET name = ?, infomation = ?, price = ?, image = ? WHERE id = ?',
+        [name, infomation, price, image, id]
+    );
+
+    return res.redirect('/Adminproduct');
+}
 /////////////////////////////////////////USERS//////////////////////////
 let getDetailPage = async (req, res) => {
     let userId = req.params.id;
@@ -70,11 +103,19 @@ let getEditPage = async (req, res) => {
 }
 let postUpdateUser = async (req, res) => {
     let { firstName, lastName, email, address, id } = req.body;
+    let img = req.file ? req.file.filename : ""; // Gán giá trị mặc định nếu không có tệp ảnh
 
-    await pool.execute('update users set firstName= ?, lastName = ? , email = ? , address= ? where id = ?',
-        [firstName, lastName, email, address, id]);
+    // Kiểm tra xem tệp ảnh đã tải lên thành công hay chưa
+    if (req.file) {
+        // Di chuyển tệp ảnh vào thư mục public/image trên máy chủ
+        fs.renameSync(req.file.path, appRoot + "/src/public/image/" + img);
+    }
 
-    return res.redirect('/');
+    // Cập nhật thông tin người dùng trong cơ sở dữ liệu
+    await pool.execute('UPDATE users SET firstName = ?, lastName = ?, email = ?, address = ?, img = ? WHERE id = ?',
+        [firstName, lastName, email, address, img, id]);
+
+    return res.redirect('/employees');
 }
 
 let getEmployeeFilePage = async (req, res) => {
@@ -90,6 +131,6 @@ let getEmployeeFilePage = async (req, res) => {
 
 
 module.exports = {
-    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser, getEmployeeFilePage,getProductDetailPage,createNewProduct,
-    getCreateProductPage
+    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser, getEmployeeFilePage, getProductDetailPage, createNewProduct,
+    getCreateProductPage, getAdminproduct, deleteProduct, getEditProduct, postUpdateProduct
 }
--- src/route/web.js
+++ src/route/web.js
@@ -36,14 +36,17 @@ const initWebRoute = (app) => {
     router.get('/detail/product/:id', homeController.getProductDetailPage)
     router.get('/create', homeController.getCreateProductPage);
     router.post('/create-new-product', upload.single('image'), homeController.createNewProduct);
-
+    router.get('/Adminproduct', homeController.getAdminproduct);
+    router.post('/delete-product', homeController.deleteProduct);
+    router.get('/edit-product/:id', homeController.getEditProduct);
+    router.post('/update-product', [upload.single('image'), homeController.postUpdateProduct]);
     ////////////////////////////////////////////////////////////
     router.get('/detail/user/:id', homeController.getDetailPage)
     router.post('/create-new-user', upload.single('img'), homeController.createNewUser);
     router.post('/delete-user', homeController.deleteUser);
     router.get('/edit-user/:id', homeController.getEditPage);
-    router.post('/update-user', homeController.postUpdateUser);
-    
+    router.post('/update-user', [upload.single('img'), homeController.postUpdateUser]);
+
     router.get('/employees', homeController.getEmployeeFilePage);
 
 
